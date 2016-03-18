7c72f29d83a640f93862f00472b00d93bb78f3a4
Tzky <tzk.forums@gmail.com>
2016-03-18 14:19:19 +0100

    adjust theme to build without being a submodule, but a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ehsphinx',
+    version='0.0.1',
+    packages=['ehsphinx'],
+    url='',
+    license='',
+    author='EngineHub Team',
+    author_email='',
+    description='EngineHub Sphinx Theme'
+)
